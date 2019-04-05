18ec7a65f116d0b30c27cab10539b75d7e153027
Tyzzz <tao.hou.gr@dartmouth.edu>
2019-04-05 00:37:16 -0400

    add date

--- MyRun/myrun2/app/src/main/java/com/example/myrun2/ListAdapter.java
+++ MyRun/myrun2/app/src/main/java/com/example/myrun2/ListAdapter.java
@@ -7,43 +7,39 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 public class ListAdapter extends ArrayAdapter {
 
-    //to reference the Activity
     private final Activity context;
-
-    //to store the list of countries
-    private final String[] nameArray;
-
-    //to store the list of countries
-    private final String[] infoArray;
+    private final String[] Options;
+    private final String[] Results;
 
 
-    public ListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam){
+    ListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam){
 
-        super(context,R.layout.listview_row , nameArrayParam);
+        super(context, R.layout.listview_row , nameArrayParam);
         this.context = context;
-        this.nameArray = nameArrayParam;
-        this.infoArray = infoArrayParam;
+        this.Options = nameArrayParam;
+        this.Results = infoArrayParam;
 
     }
 
-
-    public View getView(int position, View view, ViewGroup parent) {
+    @NonNull
+    @Override
+    public View getView(int position,View view, @NonNull ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.listview_row, null,true);
 
         //this code gets references to objects in the listview_row.xml file
-        TextView nameTextField = rowView.findViewById(R.id.manual_option);
-        TextView infoTextField =  rowView.findViewById(R.id.manual_result);
+        TextView mOPtionsView = rowView.findViewById(R.id.manual_option);
+        TextView mResultsView =  rowView.findViewById(R.id.manual_result);
 
         //this code sets the values of the objects to values from the arrays
-        nameTextField.setText(nameArray[position]);
-        infoTextField.setText(infoArray[position]);
+        mOPtionsView.setText(Options[position]);
+        mResultsView.setText(Results[position]);
 
         return rowView;
 
-    };
-
-
+    }
 }
--- MyRun/myrun2/app/src/main/java/com/example/myrun2/Manal_Entry.java
+++ MyRun/myrun2/app/src/main/java/com/example/myrun2/Manal_Entry.java
@@ -1,5 +1,6 @@
 package com.example.myrun2;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -9,10 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.Calendar;
 import java.util.Objects;
+
 
 public class Manal_Entry extends AppCompatActivity {
 
@@ -20,6 +24,8 @@ public class Manal_Entry extends AppCompatActivity {
     String[] mOptions = {"Activity","Date","Time","Duration","Distance","Calorie", "Heartbeat","Comment"};
     String[] mResults = {"Manual", "2019-01-01", "10:10", "0 mins", "0 kms", "0 cals", "0 bpm", " "};
     private ListView mlistView;
+    private Calendar mDateTime = Calendar.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +38,36 @@ public class Manal_Entry extends AppCompatActivity {
 
 
 
-        ListAdapter whatever = new ListAdapter(this, mOptions, mResults);
+        ListAdapter la = new ListAdapter(this, mOptions, mResults);
         mlistView = findViewById(R.id.manual_listview);
-        mlistView.setAdapter(whatever);
+        mlistView.setAdapter(la);
 
 
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
                 Log.d(TAG, String.valueOf(position));
 
                 switch (position){
-                    case 0:
+                    case 1:
+                        onDateClick();
                          break;
 
                     case 3:
                     case 4:
-//                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
                         AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(Manal_Entry.this);
-                        builder.setTitle("Title");
-// I'm using fragment here so I'm using getView() to provide ViewGroup
-// but you can provide here any other instance of ViewGroup from your Fragment / Activity
+                        builder.setTitle(mOptions[position]);
                         View viewInflated = LayoutInflater.from(getApplicationContext()).inflate(R.layout.input_dialog, null);
-// Set up the input
-                        final EditText input = (EditText) viewInflated.findViewById(R.id.edit_text);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                        final EditText input =  viewInflated.findViewById(R.id.edit_text);
                         builder.setView(viewInflated);
 
-// Set up the buttons
                         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
                                 String m_Text = input.getText().toString();
+                                mResults[position] = m_Text;
+
                             }
                         });
                         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
@@ -96,14 +99,26 @@ public class Manal_Entry extends AppCompatActivity {
 
     }
 
+    public void onDateClick() {
 
+        DatePickerDialog.OnDateSetListener mDateListener = new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int month,
+                                  int day) {
+                mDateTime.set(Calendar.YEAR, year);
+                mDateTime.set(Calendar.MONTH, month);
+                mDateTime.set(Calendar.DAY_OF_MONTH, day);
+//                String tmp = DateUtils.formatDateTime(getApplicationContext(),
+//                        mDateTime.getTimeInMillis(), DateUtils.FORMAT_SHOW_DATE
+//                                | DateUtils.FORMAT_SHOW_TIME);
+                Log.d(TAG,  String.valueOf(year));
+            }
+        };
+        new DatePickerDialog(Manal_Entry.this, mDateListener,
+                mDateTime.get(Calendar.YEAR),
+                mDateTime.get(Calendar.MONTH),
+                mDateTime.get(Calendar.DAY_OF_MONTH)).show();
 
-
-
-
-
-
-
+    }
 
 
 }
