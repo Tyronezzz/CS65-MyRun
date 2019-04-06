46c398e76ee5aac4cce60a70456f68aaae05e930
Tyzzz <tao.hou.gr@dartmouth.edu>
2019-04-06 12:56:50 -0400

    codeit2 swip?

--- CodeIt/debug_this/app/src/main/java/edu/dartmouth/cs/actiontabs/ActionTabsViewPagerAdapter.java
+++ CodeIt/debug_this/app/src/main/java/edu/dartmouth/cs/actiontabs/ActionTabsViewPagerAdapter.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.util.Log;
+import android.view.MotionEvent;
 
 import java.util.ArrayList;
 
